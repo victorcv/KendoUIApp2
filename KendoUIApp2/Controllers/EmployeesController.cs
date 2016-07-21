572aaa1e469162c5c3203da47f0dd58d3552e0d6
victorcv <victorcorreasvillas@gmail.com>
2016-07-21 12:54:38 +0200

    Add project files.

---
+++ KendoUIApp2/Controllers/EmployeesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KendoUIApp2.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        readonly Models.NorthwindEntities _context = new Models.NorthwindEntities();
+        // GET: Employees
+        public ActionResult Index()
+        {
+            var employees = _context.Employees.Select(e => new
+            {
+                Id = e.EmployeeID,
+                Name = e.FirstName + " " + e.LastName
+            });
+
+            return this.Json(employees,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ KendoUIApp2Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KendoUIApp2Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
